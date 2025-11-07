c1190d712c0cec230e0161593ef28edbee01b41c
aimanebreda <aimarnebreda16@gmail.com>
2025-11-07 18:21:13 +0100

    anadir colas de sagas

--- app_payment/broker/setup_rabbitmq.py
+++ app_payment/broker/setup_rabbitmq.py
@@ -1,13 +1,21 @@
-from microservice_chassis_grupo2.core.rabbitmq_core import get_channel, declare_exchange
+from microservice_chassis_grupo2.core.rabbitmq_core import get_channel, declare_exchange, declare_exchange_saga, declare_exchange_command
 
 async def setup_rabbitmq():
     connection, channel = await get_channel()
     
     exchange = await declare_exchange()
+    exchange_saga = await declare_exchange_saga()
+    exchange_command = await declare_exchange_command()
 
     payment_queue = await channel.declare_queue('payment_queue', durable=True)
 
     await payment_queue.bind(exchange, routing_key='order.created')
+
+    return_money_queue = await channel.declare_queue('return_money_queue', durable=True)
+    await return_money_queue.bind(exchange_command, routing_key='return.money')
+
+    pay_queue = await channel.declare_queue('pay_queue', durable=True)
+    await pay_queue.bind(exchange_command, routing_key='pay')
 
     print("✅ RabbitMQ configurado correctamente (exchange + colas creadas).")
 
